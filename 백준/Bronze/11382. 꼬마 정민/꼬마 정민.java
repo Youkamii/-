aca98b20622c88bd5828c259fce3d17ef1997569
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-02-28 18:31:56 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -6,9 +6,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long A = Long.parseLong(st.nextToken());
-        long B = Long.parseLong(st.nextToken());
-        long C = Long.parseLong(st.nextToken());
-        System.out.print(A + B + C);
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());    
+        
+        System.out.print(a + b + c);
     }
 }
