11c69e57187f3b3f6271d1c693903d0291ca28a6
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-03-04 10:54:08 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.54 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {       
+        int[] answer = new int[targets.length];
+        Map<Character, Integer> typeBoard = new HashMap<>();
+        
+        for (int k = 0; k < keymap.length; k++) {           
+            String key = keymap[k];            
+            
+            for (int i = 0; i < key.length(); i++) {
+                char c = key.charAt(i);
+                
+                if (!typeBoard.containsKey(c) || typeBoard.get(c) > i +1) {
+                    typeBoard.put(c, i + 1);
+                }
+            }
+        }
+        
+        for (int k = 0; k < targets.length; k++) {           
+            String t = targets[k];         
+            int count = 0;
+            
+            for (int i = 0; i < t.length(); i++) {
+                char c = t.charAt(i);
+                
+                if (!typeBoard.containsKey(c)) {
+                    count = -1;
+                    break;
+                } else {
+                    count += typeBoard.get(c);
+                }
+            
+            }
+            answer[k] = count;
+        }
+        return answer;
+    }            
+}
