9f9b6b8010235933097a804bc0065ead12918174
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-03-12 12:36:55 +0900

    [level 2] Title: N-Queen, Time: 237.17 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,16 +1,58 @@
 import java.util.*;
 
 class Solution {
-    
+
     int answer = 0;
     boolean[][] table;
-    
+
     public int solution(int n) {
-        
         table = new boolean[n][n];
-        
-        System.out.println(table[1][1]);
-        
+        placeQueens(0, n);
         return answer;
+    }
+
+    private void placeQueens(int row, int n) {
+        if (row == n) {
+            answer++;
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (checkingPossible(row, col, n)) {
+                table[row][col] = true;
+                placeQueens(row + 1, n);
+                table[row][col] = false;
+            }
+        }
+    }
+
+    public boolean checkingPossible (int row, int col, int n) {
+        
+        if (!checkY(row, col, n))
+            return false;
+        else if (!checkD(row, col, n))
+            return false;
+        
+        return true;
+    }
+
+    private boolean checkY(int row, int col, int n) {
+        for (int i = 0; i < row; i++) {
+            if (table[i][col]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkD(int row, int col, int n) {
+        for (int i = 1; i <= row; i++) {
+            if (col - i >= 0 && table[row - i][col - i]) {
+                return false;
+            }
+            if (col + i < n && table[row - i][col + i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
