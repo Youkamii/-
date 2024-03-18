00051570de4c974b63ee3ba3876fbc6d30ada5bf
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-03-19 02:46:00 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int answer = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs104(computers, visited, i);
+                answer++;
+            }
+        }
+        return answer;
+    }    
+    private void dfs104(int[][] computers, boolean[] visited, int start) {
+        visited[start] = true;
+        
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[start][i] == 1 && !visited[i]) {
+                dfs104(computers, visited, i);
+            }
+        }
+    }
+}
