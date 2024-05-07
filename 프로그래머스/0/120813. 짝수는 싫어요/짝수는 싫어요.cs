f54d6b99196c4459cdbab9e1191b9f7c5e2b3e95
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-05-07 17:41:48 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.17 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -2,17 +2,7 @@ using System;
 
 public class Solution {
     public int[] solution(int n) {
-        int ArraySize = (n + 1) / 2;
-        int[] answer = new int[ArraySize];
-        int j = 0;
-        
-        for (int i = 1; i <= n; i ++) {
-            if (i % 2 != 0) {
-                answer[j] = i;
-                j++;
-            }
-        }
-        
+        int[] answer = new int[] {};
         return answer;
     }
 }
