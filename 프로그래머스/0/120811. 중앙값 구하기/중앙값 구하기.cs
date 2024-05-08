7abc8b61bc94de31738b9122a12ad7fb1cdaad5e
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-05-08 22:48:01 +0900

    [level 0] Title: 중앙값 구하기, Time: 1.80 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        Array.Sort(array);
+            
+        return array[array.Length / 2];
+    }
+}
