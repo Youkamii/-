32c580247b4f88961ae16ee239d6e9834e3d8644
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-05-10 17:28:26 +0900

    [level 0] Title: 각도기, Time: 0.15 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution {
+    public int solution(int angle) {
+        if ( angle < 90)
+        {
+            return 1; 
+        }
+        else if (angle == 90)
+        {
+            return 2;
+        }
+        else if ( angle < 180)
+        {
+            return 3;
+        }
+        else
+        {
+            return 4;
+        }
+        
+
+    }
+}
