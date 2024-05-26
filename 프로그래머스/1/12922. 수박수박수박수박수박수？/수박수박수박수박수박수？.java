00caed8f876cd02421f9ad172ff397602997e0ca
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-05-26 23:22:42 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.24 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+
+		for (int i = 0; i < n; i++) 			
+			answer += (i % 2 == 0) ? "수" : "박";
+			
+		return answer;
+    }
+}
