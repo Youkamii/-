43f85565427d75a99028c44ba7a16ad1070c4af6
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-05-29 12:28:07 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.15 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        if (num1 == num2)
+            return 1;
+        else if (num1 != num2)
+            return -1;
+         
+        return 0;
+    }
+}
