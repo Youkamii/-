b46771e3b3bbc91018d9b6e89c1da8d7764b0fc0
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-06-04 20:21:55 +0900

    [level 0] Title: 가위 바위 보, Time: 0.26 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cs
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cs
@@ -1,8 +1,19 @@
 using System;
-
+using System.Text;
+// string ~= char[]            list.Add();
 public class Solution {
     public string solution(string rsp) {
-        string answer = "";
-        return answer;
+        StringBuilder StrB = new StringBuilder();
+
+        for (int i = 0; i < rsp.Length; i++) {
+            if (rsp[i] == '2')
+                StrB.Append("0");
+            else if (rsp[i] == '5')
+                StrB.Append("2");
+            else
+                StrB.Append("5");      
+        }
+        
+        return StrB.ToString();
     }
 }
