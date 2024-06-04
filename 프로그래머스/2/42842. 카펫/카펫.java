dc662991fe3170afb8c2ae98f5c4e12cf8a3e432
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-06-05 08:12:28 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,8 +3,7 @@ class Solution {
         int x = brown / 2 - 3;
         
         for (int y = 1; y <= x; y++) {
-            if (x * y == yellow)
-                return new int[]{x + 2, y + 2};
+            if (x * y == yellow) return new int[]{x + 2, y + 2};
             x--;
         }
         return null;
