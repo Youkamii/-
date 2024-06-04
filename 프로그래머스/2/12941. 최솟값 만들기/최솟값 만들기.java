e0da5315fa008235f0cf5156fe47f44062b9fc44
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-06-05 07:54:29 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.52 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,17 +1,15 @@
 import java.util.*;
-
 class Solution
 {
     public int solution(int []A, int []B)
     {
+        int answer = 0;
         Arrays.sort(A);
-		Arrays.sort(B);
-		int answer = 0;		
-		int length = A.length - 1;
+        Arrays.sort(B);
+        int l = B.length - 1;
+        
+        for (int i = 0; i < A.length; i++) answer += A[i] * B[l - i];
 
-		for (int i = 0; i <= length; i++)
-			answer += A[i] * B[length - i];
-
-		return answer;
+        return answer;
     }
 }
