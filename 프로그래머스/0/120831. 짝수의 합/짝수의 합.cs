38f2fc8ad582e18ff54c284b9563d35516488d47
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-06-04 19:39:29 +0900

    [level 0] Title: 짝수의 합, Time: 0.24 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
@@ -1,15 +1,13 @@
 using System;
 
+// for, if 문
+
 public class Solution {
     public int solution(int n) {
-        int answer = 0; 
-        
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                answer += i;
-            }
-        }
-       return answer; 
-        
+        int answer = 0;
+
+        for (int i = 0; i <= n; i += 2) answer += i;
+
+        return answer;
     }
 }
