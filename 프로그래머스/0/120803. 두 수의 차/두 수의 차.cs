0eba0a6d60cf27146fbc5993bbe0ec98eb0ca409
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-06-04 18:54:54 +0900

    [level 0] Title: 두 수의 차, Time: 0.12 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.cs
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.cs
@@ -1,7 +1,11 @@
 using System;
 
 public class Solution {
-    public int solution(int num1, int num2) {    
-        return num1 - num2;
+    public int solution(int num1, int num2) {
+        int answer = 0;
+
+        answer = num1 - num2;
+
+        return answer;
     }
 }
