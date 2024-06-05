9a84505439896cde5e4a421bbc47c760e1a2eecb
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-06-05 21:22:18 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.25 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cs
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cs
@@ -1,13 +1,8 @@
 using System;
 
 public class Solution {
-    public int solution(int[] num_list) {
-
-        for (int i = 0; i < num_list.Length; i++) {
-            if (num_list[i] < 0)
-                return i;
-        }
+    public int solution(int[] num_list) {        
+        for (int i = 0; i < num_list.Length; i++) if (num_list[i] < 0) return i;
         return -1;
-        
     }
 }
