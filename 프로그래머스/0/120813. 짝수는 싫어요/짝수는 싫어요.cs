192a8ad4115db1d8e88f66f433f89482a72a1ba9
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-06-07 14:44:32 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.84 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 public class Solution {
     public int[] solution(int n) {
         List<int> answer = new List<int>();
-            // 1 3 5 7 9 
-        for (int i = 1; i <= n; i += 2)
-            answer.Add(i);            
+        // 1 3 5 7 9 11 13 ...
+        for (int i = 1; i <= n; i += 2) {
+                answer.Add(i);
+        }
 
         return answer.ToArray();
     }
-}
+} // 오름차순 = sort 정렬 35179
