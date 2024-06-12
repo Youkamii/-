62ad4fa7b189c537b52d9214494a7006bc43a72f
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-06-12 20:25:54 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.54 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.cs
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 public class Solution {
     public int[] solution(int n, int[] numlist) {
-        List<int> MyList = new List<int>();
+        List<int> answer = new List<int>();
         
         for (int i = 0; i < numlist.Length; i++) {
-            if (numlist[i] % n == 0) {
-                MyList.Add(numlist[i]);
-            }
+            if (numlist[i] % n == 0)
+                answer.Add(numlist[i]);
         }
-        return MyList.ToArray();
+        
+        return answer.ToArray();
     }
 }
