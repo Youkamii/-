6feeb365527b02b61a121bf89e5c42262fa6a3b8
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-06-17 21:05:24 +0900

    [level 0] Title: 가위 바위 보, Time: 0.23 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cs
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cs
@@ -1,22 +1,19 @@
 using System;
 using System.Text;
 
-// C# 에서는 문자열을 char[] 취급 가능하다.
-
 public class Solution {
     public string solution(string rsp) {
-        StringBuilder StrB = new StringBuilder();
-        // List<int> list = new List<int>(); -> Add
-        
-        for(int i = 0; i < rsp.Length; i++) {
+        StringBuilder answer = new StringBuilder();
+          
+        for (int i = 0; i < rsp.Length; i++) {
             if (rsp[i] == '2')
-                StrB.Append("0");
-            else if (rsp[i] == '5')
-                StrB.Append("2");
+                answer.Append('0');
+            else if (rsp[i] == '0')
+                answer.Append('5');
             else
-                StrB.Append("5");
+                answer.Append('2');
         }
         
-        return StrB.ToString();
+        return answer.ToString();
     }
 }
