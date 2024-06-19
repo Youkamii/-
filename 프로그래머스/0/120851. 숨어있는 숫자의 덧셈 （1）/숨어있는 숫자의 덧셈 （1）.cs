af9164123830cab91369ef7c112e2b7c1f3bc7e1
Hyeok choi <87900502+Youkamii@users.noreply.github.com>
2024-06-19 11:35:00 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.25 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cs
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cs
@@ -3,15 +3,13 @@ using System;
 public class Solution {
     public int solution(string my_string) {
         int answer = 0;
-        // "aAb1B2cC34oOp" -> [a,A,b,1,B,2,c,C,3,4,o,O,p]
+        
         for (int i = 0; i < my_string.Length; i++) {
-            char c = my_string[i];
-            
-            if (c >= 48 && c <= 57) {
-                answer += c - '0';
-                            
-            }
-        }        
+             
+            if (my_string[i] >= '1' && my_string[i] <= '9')
+                answer += my_string[i] - '0';
+        }
+        
         return answer;
     }
 }
