e71e6a794df4525b6285ff7d45261d8fcc78739b
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-07-25 15:50:10 +0900

    [level 0] Title: 중앙값 구하기, Time: 1.58 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cs
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cs
@@ -3,7 +3,7 @@ using System;
 public class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        
+         
         Array.Sort(array);
             
         return array[array.Length / 2];
