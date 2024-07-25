284245f358c3672f5598a9a62c95a1f1b1a9ac1d
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-07-25 15:49:54 +0900

    [level 0] Title: 짝수의 합, Time: 0.16 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
@@ -2,10 +2,14 @@ using System;
 
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int answer = 0;  
         
-        for (int i = 0; i <= n; i += 2) answer += i;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                answer += i;
+            }
+        }
+       return answer; 
         
-        return answer;
     }
 }
