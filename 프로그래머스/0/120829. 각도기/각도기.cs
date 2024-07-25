d36bb273a234ade91840cbe65a5f5c46d51c279d
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-07-25 15:49:51 +0900

    [level 0] Title: 각도기, Time: 0.20 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.cs
+++ 프로그래머스/0/120829. 각도기/각도기.cs
@@ -2,12 +2,23 @@ using System;
 
 public class Solution {
     public int solution(int angle) {
-        if (angle < 90) // -2147484648 ~ 89
-            return 1;
-        else if ( angle == 90) 
+        if ( angle < 90)
+        { 
+            return 1; 
+        } 
+        else if (angle == 90)
+        {
             return 2;
-        else if (angle < 180) 
+        }
+        else if ( angle < 180)
+        {
             return 3;
-        return 4; // 180 ~ 2147483647
+        }
+        else
+        {
+            return 4;
+        }
+        
+
     }
 }
