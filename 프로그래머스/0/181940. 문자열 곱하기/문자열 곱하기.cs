7f8069df209498348547c311c46456291ae1b501
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-08-03 01:21:29 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.32 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
@@ -4,10 +4,13 @@ using System.Text;
 public class Solution {
     public string solution(string my_string, int k) {
         StringBuilder StrB = new StringBuilder();
-        
+         
         for (int i = 0; i < k; i++)
             StrB.Append(my_string);
         
         return StrB.ToString();
+        
+
+        
     }
 }
