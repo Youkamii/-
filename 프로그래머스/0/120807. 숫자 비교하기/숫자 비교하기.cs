c9f991dd182face52208dabd4ad1a76213c0ab2b
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-08-20 14:04:10 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.23 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cs
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -5,7 +5,7 @@ public class Solution {
         if (num1 == num2)
             return 1;
         else if (num1 != num2)
-            return -1;
+            return -1; 
          
         return 0; 
     }
