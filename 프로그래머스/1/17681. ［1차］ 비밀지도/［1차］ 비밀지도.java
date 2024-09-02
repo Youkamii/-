d0c18fdcf1ff0f9db9bf13676e01f627a108353b
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-09-02 11:43:03 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.09 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,27 +1,29 @@
 import java.util.*;
 
-// class Solution {
-//     public String[] solution(int n, int[] arr1, int[] arr2) {
-//         String[] answer = new String[n];        
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];        
         
-//         for (int i = 0; i < n; i++) answer[i] = D2B(n, arr1[i], arr2[i]);
+        for (int i = 0; i < n; i++) answer[i] = D2B(n, arr1[i], arr2[i]);
         
-//         return answer;
-//     }
-    
-//     public String D2B(int n, int cipher1, int cipher2) {
-//         StringBuilder code = new StringBuilder();
+        return answer;
+    }
+      
+    public String D2B(int n, int cipher1, int cipher2) {
+        StringBuilder code = new StringBuilder();
         
-//         for (int i = 0; i < n; i++) {
-//             int bit = 1 << (n - 1 - i);            
-//             if ((cipher1 & bit) != 0 || (cipher2 & bit) != 0) code.append("#");
-//             else code.append(" ");
-//         }
+        for (int i = 0; i < n; i++) {
+            int bit = 1 << (n - 1 - i);            
+            if ((cipher1 & bit) != 0 || (cipher2 & bit) != 0)
+                code.append("#");
+            else
+                code.append(" ");
+        }
         
-//         return code.toString();
-//     }
-// }
-
+        return code.toString();
+    }
+}
+   
 // class Solution {
 //   public String[] solution(int n, int[] arr1, int[] arr2) {
 //         String[] result = new String[n];
@@ -38,21 +40,4 @@ import java.util.*;
 //         return result;
 //     }
 // }
-
-class Solution {
-  public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] result = new String[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-        }
-
-        for (int i = 0; i < n; i++) {
-            result[i] = String.format("%" + n + "s", result[i]);
-            result[i] = result[i].replaceAll("1", "#");
-            result[i] = result[i].replaceAll("0", " ");
-        }
-
-        return result;
-    }
-}
     
