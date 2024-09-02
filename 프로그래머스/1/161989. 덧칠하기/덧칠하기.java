8ffb3669bc55d69d0b3a162effe8fb362d3f698d
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-09-03 01:49:14 +0900

    [level 1] Title: 덧칠하기, Time: 1.64 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
 		int end;
-		int done = 0;
+		int done = 0; 
 
 //		for (int i = 0; i < section.length; i++) {
 //			end = section[i] + m - 1;
