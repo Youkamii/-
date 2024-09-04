4c321092db22a239a6e3dafc07ca3cbd0984159a
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-09-05 01:30:13 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.37 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int hook : moves) {
+            int doll = crane(board, hook - 1);
+            
+            if (doll != 0) {
+                if(stack.isEmpty())
+                    stack.add(doll);
+                else {
+                    int top = stack.pop();
+                    if(doll == top) {
+                        answer += 2;
+                    } else {
+                        stack.add(top);
+                        stack.add(doll);
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    public int crane(int[][] board, int x) {
+        for (int y = 0; y < board.length; y++) {
+            if(board[y][x] != 0) {
+                int doll = board[y][x];
+                board[y][x] = 0;
+                return doll;
+            }
+        }
+        return 0;
+    }
+}
